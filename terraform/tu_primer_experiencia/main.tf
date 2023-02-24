cbd9d9c5d5e7857bb3435bd6e08e56475e33b4a5
Juan Jose Gazzola <jjgazzola@gmail.com>
2023-02-23 22:09:59 -0300

    Se suman archivos para el video Terraform, tu primer experiencia.

---
+++ terraform/tu_primer_experiencia/main.tf
@@ -0,0 +1,18 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+# VPC Creation
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
